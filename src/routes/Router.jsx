56462d2eae7d5ae9834624a27ca31e7df38163bb
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-04 19:35:34 +0600

    project setup completed and navbar creted

---
+++ src/routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout></RootLayout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
+
+export default router;
