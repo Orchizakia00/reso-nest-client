878888436b2c570162fd03e9c50a916e951332fb
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-08 19:34:17 +0600

    firebase env variable replaced

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQdAdn0Lz1dmok-uHKYE2BgmxSlhX2hVo",
-    authDomain: "reso-nest.firebaseapp.com",
-    projectId: "reso-nest",
-    storageBucket: "reso-nest.appspot.com",
-    messagingSenderId: "528135740272",
-    appId: "1:528135740272:web:42f3c467dac75de94d8db5"
+    apiKey:import.meta.env.VITE_APIKEY,
+    authDomain:import.meta.env.VITE_AUTHDOMAIN,
+    projectId:import.meta.env.VITE_PROJECTID,
+    storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+    appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
