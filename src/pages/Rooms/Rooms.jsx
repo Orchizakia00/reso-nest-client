75f35f1cf9f8306b39f8a60f7b8f63e219ed8aff
Orchizakia00 <zakiaislam290@gmail.com>
2023-11-08 17:07:06 +0600

    availability portion is done in rooms page

--- src/pages/Rooms/Rooms.jsx
+++ src/pages/Rooms/Rooms.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useLoaderData } from "react-router-dom";
 import Room from "./Room";
@@ -8,14 +8,7 @@ const Rooms = () => {
     const roomsData = useLoaderData();
 
     const [rooms, setRooms] = useState(roomsData);
-    // const [currentPage, setCurrentPage] = useState(0);
-    // const [itemsPerPage, setItemsPerPage] = useState(10);
-
-    // useEffect(() => {
-    //     fetch(`http://localhost:5000/rooms?page=${currentPage}&size=${itemsPerPage}`)
-    //         .then(res => res.json())
-    //         .then(data => setRooms(data))
-    // }, [currentPage, itemsPerPage])
+    const [availableRooms, setAvailableRooms] = useState(rooms.length);
 
     const handleSortByPriceLtH = () => {
         const sortedRooms = [...rooms].sort((a, b) => a.price - b.price);
@@ -26,27 +19,27 @@ const Rooms = () => {
         setRooms(sortedRooms);
     };
 
-    // pagination
-    // const numberOfPages = Math.ceil(rooms.length / itemsPerPage);
 
-    // const pages = [...Array(numberOfPages).keys()];
+    useEffect(() => {
+        fetch('http://localhost:5000/bookings')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
 
-    // const handleItemsPerPage = e => {
-    //     const val = parseInt(e.target.value);
-    //     setItemsPerPage(val);
-    //     setCurrentPage(0);
-    // }
+                const bookedRoomsIds = data.map(bookedRoom => bookedRoom.roomId);
 
-    // const handlePrev = () => {
-    //     if (currentPage > 0) {
-    //         setCurrentPage(currentPage - 1);
-    //     }
-    // }
-    // const handleNext = () => {
-    //     if (currentPage < pages.length - 1) {
-    //         setCurrentPage(currentPage + 1);
-    //     }
-    // }
+                const roomIds = rooms.map(room => room._id);
+
+                const hasBookedRooms = roomIds.some(roomId => bookedRoomsIds.includes(roomId));
+
+                if (hasBookedRooms) {
+                    const availableRooms = roomIds.length - bookedRoomsIds.length;
+                    setAvailableRooms(availableRooms);
+                }
+            })
+    }, [rooms])
+
+
 
 
     return (
@@ -62,41 +55,24 @@ const Rooms = () => {
                     </div>
                 </div>
             </div>
-            <div className="dropdown dropdown-bottom mb-8">
-                <label tabIndex={0} className="btn m-1 normal-case">Filter By Price</label>
-                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a onClick={handleSortByPriceLtH}>Low to High</a></li>
-                    <li><a onClick={handleSortByPriceHtL}>High to Low</a></li>
-                </ul>
+            <div className="flex justify-between mb-8">
+                <div className="dropdown dropdown-bottom">
+                    <label tabIndex={0} className="btn m-1 normal-case">Filter By Price</label>
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a onClick={handleSortByPriceLtH}>Low to High</a></li>
+                        <li><a onClick={handleSortByPriceHtL}>High to Low</a></li>
+                    </ul>
+                </div>
+                <div className="flex items-center">
+                    <p><span className="font-bold">Available Rooms:</span> {availableRooms} </p>
+                </div>
             </div>
-            {/* <h1 className="text-center text-4xl font-bold my-10">Our Rooms</h1> */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 {
                     rooms.map(room => <Room key={room._id} room={room}></Room>)
                 }
             </div>
 
-            {/* pagination */}
-            {/* <div className="mt-10 text-center">
-                <p className="mb-4">Current Page: {currentPage} </p>
-                <button onClick={handlePrev} className="btn btn-base normal-case">Prev</button>
-                {
-                    pages.map(page => (
-                        <button
-                            onClick={() => setCurrentPage(page)}
-                            key={page}
-                            className={currentPage == page ? 'btn-accent btn mx-2' : 'btn btn-base mx-2'} >
-                            {page}
-                        </button>
-                    ))
-                }
-                <button onClick={handleNext} className="btn btn-base normal-case">Next</button>
-                <select value={itemsPerPage} onChange={handleItemsPerPage} name="" id="">
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                </select>
-            </div> */}
         </div>
     );
 };
